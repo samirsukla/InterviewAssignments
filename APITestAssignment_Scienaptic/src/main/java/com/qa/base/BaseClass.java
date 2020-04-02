fd12f3723c04548df5163976ada884ed48cc217b
samirsukla <sk.sukla@outlook.com>
2020-04-02 13:37:37 +0530

    Designed the Framework Structure

---
+++ APITestAssignment_Scienaptic/src/main/java/com/qa/base/BaseClass.java
@@ -0,0 +1,9 @@
+package com.qa.base;
+
+import org.testng.annotations.Test;
+
+public class BaseClass {
+  @Test
+  public void f() {
+  }
+}
---
+++ APITestAssignment_Scienaptic/src/test/java/com/qa/unitTest/SignUpTest.java
@@ -0,0 +1,9 @@
+package com.qa.unitTest;
+
+import org.testng.annotations.Test;
+
+public class SignUpTest {
+  @Test
+  public void f() {
+  }
+}
